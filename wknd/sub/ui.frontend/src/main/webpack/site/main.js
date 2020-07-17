6d9feee94647c61afadeab331c197991c64cd1ab
Carsten <cziegele@adobe.com>
2020-07-17 08:45:15 +0000

    Update wknd

---
+++ wknd/sub/ui.frontend/src/main/webpack/site/main.js
@@ -0,0 +1,11 @@
+// Stylesheets
+import './main.scss';
+
+// Typescript/Javascript
+import './util.js';
+import './scroll-indicator';
+import '../components/languagenavigation/languagenavigation.js';
+import '../components/layout-container/modal.js';
+import '../components/navigation/navigation.js';
+import '../components/form/sign-in-buttons/sign-in-buttons.js';
+import '../components/form/sign-in-form/sign-in-form.js';
